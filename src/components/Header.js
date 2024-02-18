336c92e32247cb9b1465968168cf726a411040fe
Advaith <advaith1601@gmail.com>
2024-02-18 11:50:47 +0530

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
   const { loading } = useContext(VideoContext);
   return (
-    <div className="sticky top-0 z-10 flex flex-row items-center justify-center h-14 px-4 md:px-5 bg-white dark:bg-black">
+    <div className="sticky top-0 z-10 flex flex-row items-center justify-center h-14 px-4 md:px-5 bg-black">
       {loading && <Loader />}
       <div className="flex h-5 items-center">
         <Link to={`/`}>
